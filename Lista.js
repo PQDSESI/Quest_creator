cb9f1eecd90c0d6b13df1765d58eaeddb8f9aaef
PQDSESI <gabsouzafer@hotmail.com>
2026-01-09 17:01:16 -0300

    adicionando node e modulos do node prettier/eslint e formatando codigo

--- Lista.js
+++ Lista.js
@@ -1,25 +1,24 @@
 const dados = JSON.parse(sessionStorage.getItem("dadosLista"));
 
 if (!dados) {
-    alert("Dados da lista não encontrados.");
-    window.location.href = "index.html";
+  alert("Dados da lista não encontrados.");
+  window.location.href = "index.html";
 }
 
 document.getElementById("infoLista").innerText =
-    `${dados.materia} - ${dados.lista} - ${dados.tipoLista} - ${dados.nivel}`;
+  `${dados.materia} - ${dados.lista} - ${dados.tipoLista} - ${dados.nivel}`;
 
-    const container = document.getElementById("questoes-container");
-    const numinicio = Number(dados.inicio);
-    const numfim = Number(dados.fim);
+const container = document.getElementById("questoes-container");
+const numinicio = Number(dados.inicio);
+const numfim = Number(dados.fim);
 
-    const total = Number(numfim - numinicio +1);
+const total = Number(numfim - numinicio + 1);
 
-    for (let i =  numinicio, j = numfim ; i <= j; i++) {
+for (let i = numinicio, j = numfim; i <= j; i++) {
+  const bloco = document.createElement("div");
+  bloco.style.marginBottom = "15px";
 
-        const bloco = document.createElement("div");
-        bloco.style.marginBottom = "15px";
-    
-        bloco.innerHTML = `
+  bloco.innerHTML = `
             <strong>Questão ${i}</strong><br>
             <div class="opcoes">
             <label>
@@ -46,123 +45,124 @@ document.getElementById("infoLista").innerText =
 
 
         `;
-    
-        container.appendChild(bloco);
-    }
+
+  container.appendChild(bloco);
+}
 
 function closessesion() {
-    sessionStorage.clear();
+  sessionStorage.clear();
 }
 
 function gerarTexto(dados, dadosquestao) {
-    let texto = "";
+  let texto = "";
 
-    texto += `Matéria: ${dados.materia}\n`;
-    texto += `Lista: ${dados.lista}\n`;
-    texto += `Tipo: ${dados.tipoLista}\n`;
-    texto += `Data: ${dados.data}\n\n`;
-    texto += `Nível: ${dados.nivel}\n\n`;
+  texto += `Matéria: ${dados.materia}\n`;
+  texto += `Lista: ${dados.lista}\n`;
+  texto += `Tipo: ${dados.tipoLista}\n`;
+  texto += `Data: ${dados.data}\n\n`;
+  texto += `Nível: ${dados.nivel}\n\n`;
 
-    texto += "QUESTÕES\n";
-    texto += "--------------------------------\n";
+  texto += "QUESTÕES\n";
+  texto += "--------------------------------\n";
 
-    dadosquestao.forEach(r => {
-        texto += `Q${r.questao} | Marcada: ${r.marcada} | Correta: ${r.correta ? r.correta : "-"} | ${r.resultado}\n`;
-    });
+  dadosquestao.forEach((r) => {
+    texto += `Q${r.questao} | Marcada: ${r.marcada} | Correta: ${r.correta ? r.correta : "-"} | ${r.resultado}\n`;
+  });
 
-    const acertos = dadosquestao.filter(r => r.resultado === "V").length;
+  const acertos = dadosquestao.filter((r) => r.resultado === "V").length;
 
-    texto += `Resultado final: ${acertos} / ${dadosquestao.length}\n`;
+  texto += `Resultado final: ${acertos} / ${dadosquestao.length}\n`;
 
-
-    return texto;
+  return texto;
 }
 
 function baixarTXT(texto, nome) {
-    const blob = new Blob([texto], { type: "text/plain;charset=utf-8" });
-    const url = URL.createObjectURL(blob);
+  const blob = new Blob([texto], { type: "text/plain;charset=utf-8" });
+  const url = URL.createObjectURL(blob);
 
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = nome;
-    a.click();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = nome;
+  a.click();
 
-    URL.revokeObjectURL(url);
+  URL.revokeObjectURL(url);
 }
 
-document.getElementById("form-questoes").addEventListener("submit", function (e) {
+document
+  .getElementById("form-questoes")
+  .addEventListener("submit", function (e) {
     e.preventDefault();
 
     const dadosquestao = [];
 
-    for (let T =  numinicio,i = 1 , j = numfim ; T <= j; T++, i++) {
+    for (let T = numinicio, i = 1, j = numfim; T <= j; T++, i++) {
+      const marcada = document.querySelector(`input[name="q${T}"]:checked`);
+      const respostaUsuario = marcada ? marcada.value : "-";
+      const correta = dados.gabarito[i - 1];
 
-        const marcada = document.querySelector(`input[name="q${T}"]:checked`);
-        const respostaUsuario = marcada ? marcada.value : "-";
-        const correta = dados.gabarito[i - 1];
-
-        dadosquestao.push({
-            questao: T,
-            marcada: respostaUsuario,
-            correta: correta,
-            resultado: respostaUsuario === correta ? "V" : "F"
-        });
-       
+      dadosquestao.push({
+        questao: T,
+        marcada: respostaUsuario,
+        correta: correta,
+        resultado: respostaUsuario === correta ? "V" : "F",
+      });
     }
 
-    const nome = `${dados.data} - (${dados.nivel}) ${dados.materia} ${dados.lista}.txt`
+    const nome = `${dados.data} - (${dados.nivel}) ${dados.materia} ${dados.lista}.txt`;
     const textofeito = gerarTexto(dados, dadosquestao);
-    baixarTXT(textofeito,nome);
-});
+    baixarTXT(textofeito, nome);
+  });
 
 const Passa = document.getElementById("PassaGabarito");
 const Volta = document.getElementById("VoltaGabarito");
 const Titulo = document.getElementById("NmrDaQuestaoTitulo");
 const TamanhoDoGabarito = dados.gabarito.length;
-let Numerodaquestão = numinicio ;
+let Numerodaquestão = numinicio;
 
 const PutText = (NumeroQ) => {
-    document.getElementById("gabarito-content").textContent=`${dados.gabarito[NumeroQ - numinicio]}`;
-    Titulo.textContent=` ${NumeroQ}`
-}
+  document.getElementById("gabarito-content").textContent =
+    `${dados.gabarito[NumeroQ - numinicio]}`;
+  Titulo.textContent = ` ${NumeroQ}`;
+};
 
-PutText(Numerodaquestão)
+PutText(Numerodaquestão);
 
 Passa.onclick = () => {
-    if (Numerodaquestão - numinicio +1 < TamanhoDoGabarito) {
-        Numerodaquestão++
-    } else {
-        Numerodaquestão = numinicio
-    }
-    PutText(Numerodaquestão)
-}
+  if (Numerodaquestão - numinicio + 1 < TamanhoDoGabarito) {
+    Numerodaquestão++;
+  } else {
+    Numerodaquestão = numinicio;
+  }
+  PutText(Numerodaquestão);
+};
 
 Volta.onclick = () => {
-    if (Numerodaquestão > numinicio) {
-        Numerodaquestão--
-    } else {
-        Numerodaquestão =  numinicio + TamanhoDoGabarito -1
-    }
-    PutText(Numerodaquestão);
+  if (Numerodaquestão > numinicio) {
+    Numerodaquestão--;
+  } else {
+    Numerodaquestão = numinicio + TamanhoDoGabarito - 1;
+  }
+  PutText(Numerodaquestão);
 };
 
 const inputs = document.querySelectorAll('[type="radio"]');
 
-inputs.forEach(input => {
-    input.addEventListener("click", function () {
-        const id = this.id;
-        PutText(id)
-       const GabaritoDaMarcada=  dados.gabarito[id - numinicio]
-       const selecionada = document.querySelector(`input[name="q${id}"]:checked`).value;
-       let result
-       const box =  document.getElementById(`B-${id}`)
-if (GabaritoDaMarcada === selecionada) {
-   box.setAttribute("class", "certa");
-   box.textContent = `Você acertou, a resposta era ${GabaritoDaMarcada}`;
-} else {
-    box.setAttribute("class", "errada");
-    box.textContent = `Você errou, a resposta era ${GabaritoDaMarcada} ` ;
-}
-    });
+inputs.forEach((input) => {
+  input.addEventListener("click", function () {
+    const id = this.id;
+    PutText(id);
+    const GabaritoDaMarcada = dados.gabarito[id - numinicio];
+    const selecionada = document.querySelector(
+      `input[name="q${id}"]:checked`
+    ).value;
+    let result;
+    const box = document.getElementById(`B-${id}`);
+    if (GabaritoDaMarcada === selecionada) {
+      box.setAttribute("class", "certa");
+      box.textContent = `Você acertou, a resposta era ${GabaritoDaMarcada}`;
+    } else {
+      box.setAttribute("class", "errada");
+      box.textContent = `Você errou, a resposta era ${GabaritoDaMarcada} `;
+    }
+  });
 });
-
--- Prompt.js
+++ Prompt.js
@@ -1,26 +1,31 @@
-document.getElementById("form-descricao").addEventListener("submit", function (e) {
+document
+  .getElementById("form-descricao")
+  .addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const subject = document.querySelector('input[name="subject"]:checked').value;
+    const subject = document.querySelector(
+      'input[name="subject"]:checked'
+    ).value;
 
     const gabarito = document.getElementById("gabarito").value;
 
-    const gabaritocomarray =  gabarito.split("-").map(letra => letra.trim().toUpperCase());
+    const gabaritocomarray = gabarito
+      .split("-")
+      .map((letra) => letra.trim().toUpperCase());
 
     const dadosLista = {
-        materia: subject,
-        lista: document.getElementById("detail").value,
-        data: document.getElementById("data").value,
-        tipoLista: document.getElementById("typels").value,
-        nivel: document.getElementById("level").value,
-        gabarito: gabaritocomarray,
-        inicio: document.getElementById("questaoinicio").value,
-        fim: document.getElementById("questaofim").value,
+      materia: subject,
+      lista: document.getElementById("detail").value,
+      data: document.getElementById("data").value,
+      tipoLista: document.getElementById("typels").value,
+      nivel: document.getElementById("level").value,
+      gabarito: gabaritocomarray,
+      inicio: document.getElementById("questaoinicio").value,
+      fim: document.getElementById("questaofim").value,
     };
 
     sessionStorage.setItem("dadosLista", JSON.stringify(dadosLista));
 
     // vai para a página das questões
     window.location.href = "./Lista.html";
-});
-
+  });
