6498d3640aeeb799629bdaf01944cea46d9b11d2
ZT-TOPICS <gabsouzafer@hotmail.com>
2026-01-25 11:49:50 -0300

    Update Prompt.js

--- Scripts/Prompt.js
+++ Scripts/Prompt.js
@@ -32,5 +32,5 @@ document
     sessionStorage.setItem("dadosLista", JSON.stringify(dadosLista));
 
     // vai para a página das questões
-    window.location.href = "/Quest_creator/Htmls/Lista.html";
+    window.location.href = "../Htmls/Lista.html";
   });
